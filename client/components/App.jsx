86f96b384c59fca5a8ac929ad699867046726865
parkjm415 <pjm605@gmail.com>
2017-06-21 12:13:21 -0400

    jsx added

---
+++ client/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div stlye={{ textAlign: 'center' }}>
+				<h1>This is JP React Boilerplate</h1>
+			</div>
+		)
+	}
+}
